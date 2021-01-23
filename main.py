fd36cb6e0fd4be40aef5fbd819d7fa3503509cf9
Kevin <knphillips01@gmail.com>
2021-01-23 15:48:09 -0600

    adding more notes

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from tkinter import *
 
-window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-
-# Label
-my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-# Re-assign Label
-my_label["text"] = "New Text"
-# my_label.config(text="New Text")
-
-# Button
 def button_clicked():
   print("I got clicked")
   new_text = input.get()
   my_label.config(text=new_text)
 
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
+
+# Label
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+# Re-assign Label
+my_label["text"] = "New Text"
+# my_label.config(text="New Text")
+# my_label.pack(side="left")
+# my_label.place(x=100, y=200)
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+# Button
 button = Button(text="Click Me", command=button_clicked)
-button.pack() 
+# button.pack(side="left") 
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=3, row=0)
 
 # Entry
 input = Entry(width=15)
-input.pack()
 print(input.get())
-
-
+# input.pack(side="left")
+input.grid(column=2, row=2)
 
 
 
